e2f226be6e56127220650fc2cb6dda56dce23a3a
phonty29 <Bekmukhamet.Amedov@kaspi.kz>
2025-10-08 18:00:01 +0500

    feat: get request target

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class Main {
   public static void main(String[] args) {
@@ -25,7 +25,7 @@ public class Main {
        System.out.println("accepted new connection");
 
        // Read input stream from socket and convert it to String
-       String requestMessage, responseMessage;
+       String requestMessage, successResponseMessage, notFoundResponseMessage;
        // Then print in standard output
 
        // An InputStreamReader is a bridge from byte streams to character streams: It reads bytes and decodes them into characters using a specified charset.
@@ -33,15 +33,28 @@ public class Main {
        BufferedReader bufReader = new BufferedReader(inStreamReader);
 
        requestMessage = bufReader.readLine();
-       System.out.println("Received message:" + " " + requestMessage);
+       String desiredRequestTarget = "/index.html";
+       String requestTarget = getRequestTarget(requestMessage);
 
        PrintWriter sockOutWriter = new PrintWriter(sock.getOutputStream(), true);
-       responseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+       successResponseMessage = "HTTP/1.1 200 OK\r\n\r\n";
+       notFoundResponseMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-       System.out.println("Response message:" + " " + responseMessage);
-       sockOutWriter.println(responseMessage);
+       if (desiredRequestTarget.contentEquals(requestTarget)) {
+         sockOutWriter.println(successResponseMessage);
+       } else {
+         sockOutWriter.println(notFoundResponseMessage);
+       }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  private static String getRequestTarget(String requestMessage) {
+    var matcher = Pattern.compile("GET (.*?) HTTP/1.1").matcher(requestMessage);
+    if (matcher.find()) {
+      return matcher.group(1);
+    }
+    return "";
   }
 }
