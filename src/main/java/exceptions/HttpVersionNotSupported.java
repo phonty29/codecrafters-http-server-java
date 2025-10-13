1124b59bd2ed26ba598ace6d35c76ab0771ab874
phonty29 <Bekmukhamet.Amedov@kaspi.kz>
2025-10-13 10:51:14 +0500

    log: remove brackets for http version not supported error

--- src/main/java/exceptions/HttpVersionNotSupported.java
+++ src/main/java/exceptions/HttpVersionNotSupported.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class HttpVersionNotSupported extends RuntimeException {
 
   public HttpVersionNotSupported(String version) {
-    super(String.format("HTTP version %s not supported. Supported versions are: [%s]", version,
+    super(String.format("HTTP version %s not supported. Supported versions are: %s", version,
         Arrays.toString(HttpVersion.values())));
   }
 }
