316d913e8e17a9ef88745153320c3c09fa9bccd4
phonty29 <Bekmukhamet.Amedov@kaspi.kz>
2025-10-20 18:24:27 +0500

    feat: decrement threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executors;
 public class Main {
   private static final ExecutorService threadPool;
   static {
-    threadPool = Executors.newFixedThreadPool(11);
+    threadPool = Executors.newFixedThreadPool(10);
   }
 
   public static void main(String[] args) {
