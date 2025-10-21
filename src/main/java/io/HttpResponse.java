b600233247dfd130a72bab37f1c4c6fb18e47946
phonty29 <Bekmukhamet.Amedov@kaspi.kz>
2025-10-21 16:35:49 +0500

    fix: check

--- src/main/java/io/HttpResponse.java
+++ src/main/java/io/HttpResponse.java
@@ -126,8 +126,7 @@ public class HttpResponse {
       if (optCompressionScheme.isPresent() && optCompressionScheme.get().equals(CompressionScheme.GZIP)) {
         byte[] compressedBody = GZIPCompressor.compress(body);
         this.httpResponse.setMessageBody(compressedBody);
-        this.httpResponse.addHeader("content-length", String.valueOf(compressedBody.length));
-        System.out.println(compressedBody.length);
+        this.httpResponse.addHeader("content-length", String.valueOf(body.length()));
       } else {
         this.httpResponse.setMessageBody(body);
         this.httpResponse.addHeader("content-length", String.valueOf(body.length()));
