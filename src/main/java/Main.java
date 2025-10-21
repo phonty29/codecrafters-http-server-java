ecc745356448b8019651f90edcded7576277e8c4
phonty29 <Bekmukhamet.Amedov@kaspi.kz>
2025-10-21 05:55:42 +0500

    refactor: remove line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,6 @@ public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
